09009a30a0450a7beae300a7a1cb25b20f8f6465
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-09-14 10:33:07 -0700

    Moved gui files into a folder. Cleaning structure

--- gui/gui.py
+++ gui/gui.py
@@ -1,8 +1,13 @@
+import sys
 import Tkinter
+
 import easygui as eg
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-from settings import DEBUG
+from base_chat import Ui_main
 
+DEBUG = False
 msgbox = eg.msgbox
 
 def getCredentials():
@@ -15,11 +20,11 @@ def getCredentials():
     title = "Brave IRC Network"
     fieldNames = ['Username', 'Password']
     fieldValues = []  # we start with blanks for the values
-    fieldValues = eg.multpasswordbox(msg, title, fieldNames)
+    fieldValues = eg.multpasswordbox(msg, title, fieldNames) if not DEBUG else None
 
     # make sure that none of the fields was left blank
-    while 1:
-        if fieldValues == None: break
+    while True:
+        if fieldValues == None or DEBUG: break
         errmsg = ""
         for i in range(len(fieldNames)):
             if fieldValues[i].strip() == "":
@@ -36,3 +41,30 @@ def getCredentials():
     credentials = {'username': fieldValues[0], 'password': fieldValues[1]}
 
     return credentials
+
+
+
+
+
+
+class MainWindow(QMainWindow, Ui_main):
+    def message(self):
+        text = self.lineEdit.text() # get user input
+        self.lineEdit.clear() # clear user input
+        self.textEdit.append(text) # write user input to display
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+
+def runApp(MainWindow, argv=sys.argv):
+    app = QApplication(argv, True)
+    wnd = MainWindow()
+    wnd.show()
+    app.connect(app, SIGNAL("lastWindowClosed()"), app, SLOT("quit()"))
+    sys.exit(app.exec_())
+ 
+if __name__ == "__main__":
+    print getCredentials()
+    runApp(MainWindow)
