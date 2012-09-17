30246c6e76f6aac8b6ff3ad96b08e877ae1c5dc9
Luis Naranjo <luisnaranjo733@gmail.com>
2012-09-16 21:09:51 -0700

    Base client and server files now

--- client.py
+++ client.py
@@ -3,97 +3,22 @@
 ''' Brave Python IRC client
 
 # Algorithm
-1. Authenticate
+1. Authenticate # Implement last
 2. Server goes into engine loop
 3. Client queries 'online users'
 4. Client queries 'recent chat'
 5. Client enters chatterloop
 '''
 
-import sys
 import socket
 from functools import partial
-from pprint import pprint
-from multiprocessing import Process
 
 import settings
 from settings import HOST, logger
-from settings import query, send, recv, rdecode
 
 
-makeSocket = lambda: socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+send = partial(settings.send, sock)
+recv = partial(settings.recv, sock)
 
-
-def exit_(s, msg=None, exit=0):
-    s.close()
-    if msg:
-        logger.info(msg)
-    sys.exit(exit)
-
-
-def authentic(credentials):
-    s = makeSocket()
-    s.connect(HOST)
-    
-    # Convenience functions
-    send = partial(settings.send, s) # send(string)
-    recv = partial(settings.recv, s) # recv() -> Encoded string
-    rdecode = partial(settings.rdecode, s) # recv() -> Decoded string
-    die = partial(exit_, s) # exit cleanly
-
-    auth_token = query('auth', credentials)
-    send(auth_token)
-    auth = rdecode(get='auth')
-    if not auth:
-        err = 'Failed to authenticate!'
-        logger.warning(err)
-        die(exit=1)
-        authenticated = False
-    else:
-        logger.info('Login successful!')
-        authenticated = True
-
-    s.close()
-    return authenticated
-
-
-
-
-def startChat(credentials): # deprecated -> salvage and reconstruct
-    s = makeSocket()
-
-    # Convenience functions
-    send = partial(settings.send, s) # send(string)
-    recv = partial(settings.recv, s) # recv() -> Encoded string
-    rdecode = partial(settings.rdecode, s) # recv() -> Decoded string
-    die = partial(exit_, s) # exit cleanly
-    # Start connection
-    s.connect((HOST, PORT))
-
-    ## authenticate
-    auth_token = query('auth', credentials)
-    send(auth_token)
-    auth = rdecode(get='auth')
-    if not auth:
-        err = 'Failed to authenticate!'
-        logger.warning(err)
-        die(exit=1)
-    else:
-        logger.info('Login successful!')
-
-    ## get online users
-    send(query('online users'))
-    online = rdecode()
-
-    print 'Online users:'
-    pprint(online.get('online'))
-
-
-    # get recent chat
-
-    # start chatter
-
-    # End connection
-    die()
-
-
+sock.connect(HOST)
--- server.py
+++ server.py
@@ -1,129 +1,17 @@
 #!/usr/bin/env python
 
-''' Blank docstring. '''
+import socket
 
-import threading
-import os
-import hashlib
-import sqlite3
-from functools import partial
-from settings import *
+import settings
+from settings import HOST, logger
 
-DB = os.path.join(os.path.dirname(__file__), 'db')
-DB = os.path.join(DB, 'braveirc.db')
-DB = os.path.abspath(DB)
-if not os.path.isfile(DB):
-    open(DB, 'a')
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SOCKET.bind(HOST)
+SOCKET.listen(1)
 
-digest = lambda password: hashlib.md5(password).hexdigest()
+clients = {}
 
-conn = sqlite3.connect(DB, check_same_thread=False)
-cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, '
-        'username VARCHAR(255), password VARCHAR(255))')
-
-conn.commit()
-
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(HOST)
-s.listen(1)
-
-
-class Client(threading.Thread):
-    def __init__(self, sc, sockname):
-        self.sc = sc
-        self.sockname = sockname
-        self.username = ''
-        self.send = partial(send, sc)
-        self.recv = partial(recv, sc)
-        self.rdecode = partial(rdecode, sc)
-        self.close = self.sc.close
-        threading.Thread.__init__(self)
-
-    def login(self):
-        token = decode(self.recv())
-        auth = token.get('auth')
-        if not auth or not isinstance(token, dict): return
-        username = auth.get('username')
-        password = auth.get('password')
-        if not username or not password: return
-
-        spassword = cur.execute('select password from users where username = ?',
-                (username,)).fetchone()
-        if not spassword: return
-        spassword = spassword[0]
-
-        self.authenticated = True if spassword == digest(password) else False
-        if self.authenticated:
-            logger.info('Authenticated: %s' % username)
-            self.username = username
-            return True
-
-    def execute(self, command):
-        logger.debug('Issuing the "%s" command.' % command)
-
-        if command == 'online users':
-            online = [client.username for client in clients if
-                    client.is_alive()]
-            response = dictencode(online=online)
-            self.send(response)
-
-
-    def run(self):
-        logger.debug('Started connection from: %s' %  str(self.sockname))
-
-        if not self.login():
-            logger.warning('Failed auth from %s' % str(self.sockname))
-            response = dictencode(auth=False)
-            self.send(response)
-            return
-        else:
-            response = encode(dict(auth=True))
-            self.send(response)
-
-        while True:
-            try:
-                request = self.rdecode()
-                logger.debug('Request: %s' % request)
-                if isinstance(request, dict):
-                    command = request.get('command')
-                    if command:
-                        self.execute(command)
-
-            except RuntimeError, tb:
-                logger.debug('Shutting down socket!')
-                break
-
-
-        self.close()
-
-clients = []
-
-
-def mainLoop():
-    while True:
-        connection = s.accept()
-        client = Client(*connection)
-        client.start()
-        client.name = connection[1]
-        clients.append(client)
-
-
-def addUser(username, password):
-    password = digest(password)
-    cur.execute('insert into users (username, password) values (?, ?)',
-            (username, password))
-    conn.commit()
-
-
-def showUsers():
-    for row in cur.execute('select * from users'):
-        print row
-
-if __name__ == '__main__':
-    try:
-        mainLoop()
-    except KeyboardInterrupt:
-        print 'Clients:', clients
-
-
+while True:
+    sock, sockname = SOCKET.accept()
+    print sockname
